cae2a593c744805a51caf32d0add5e45be26e3a6
Vinicius Borges <viniciusborgeis@gmail.com>
2024-09-02 02:10:10 -0300

    ✨ (packages/utils): add initial implementation of fs library with async and sync versions 📝 (packages/utils/package.json, packages/utils/src/fs/fileSystem.ts, packages/utils/src/fs/fileSystemAsync.ts, packages/utils/src/index.ts, packages/utils/src/string/index.ts, packages/utils/src/tsUtils/index.ts, packages/utils/tsconfig.json): add documentation for newly created files

---
+++ packages/utils/src/fs/fileSystem.ts
@@ -0,0 +1,139 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { globSync } from 'glob';
+
+// Funções relacionadas a diretórios e caminhos
+export function isDirectory(dirPath: string): boolean {
+	try {
+		return fs.lstatSync(dirPath).isDirectory();
+	} catch {
+		return false;
+	}
+}
+
+export function directoryExists(dirPath: string): boolean {
+	const resolvedPath = path.resolve(dirPath);
+	return fs.existsSync(resolvedPath);
+}
+
+export function createDirectory(dirPath: string, recursive = false): boolean {
+	try {
+		const resolvedPath = path.resolve(dirPath);
+		fs.mkdirSync(resolvedPath, { recursive });
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function deleteDirectory(dirPath: string, recursive = true): boolean {
+	try {
+		const resolvedPath = path.resolve(dirPath);
+		fs.rmdirSync(resolvedPath, { recursive });
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function getFilesByExtension(
+	dirPath: string,
+	extension = 'ts',
+): string[] {
+	try {
+		return globSync(`${dirPath}/**/*.${extension}`).map((file) =>
+			path.resolve(file),
+		);
+	} catch {
+		return [];
+	}
+}
+
+// Funções relacionadas a arquivos
+export function isFile(filePath: string): boolean {
+	try {
+		return fs.lstatSync(filePath).isFile();
+	} catch {
+		return false;
+	}
+}
+
+export function isSymbolicLink(filePath: string): boolean {
+	try {
+		return fs.lstatSync(filePath).isSymbolicLink();
+	} catch {
+		return false;
+	}
+}
+
+export function fileExists(filePath: string): boolean {
+	const resolvedPath = path.resolve(filePath);
+	return fs.existsSync(resolvedPath);
+}
+
+export function readFile(
+	filePath: string,
+	encoding: BufferEncoding = 'utf8',
+): string | null {
+	try {
+		return fs.readFileSync(filePath, encoding);
+	} catch {
+		return null;
+	}
+}
+
+export function writeFile(
+	filePath: string,
+	content: string,
+	encoding: BufferEncoding = 'utf8',
+): boolean {
+	try {
+		fs.writeFileSync(filePath, content, encoding);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function createFile(filePath: string, content: string): boolean {
+	try {
+		fs.writeFileSync(filePath, content);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function copyFile(oldFilePath: string, newFilePath: string): boolean {
+	try {
+		fs.copyFileSync(oldFilePath, newFilePath);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function deleteFile(filePath: string): boolean {
+	try {
+		fs.unlinkSync(filePath);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export function getAllFilesInDirectory(rootDir: string): string[] {
+	try {
+		return globSync(`${rootDir}/**/*`, {
+			dot: true,
+			nodir: true,
+			ignore: ['**/node_modules/**', '**/.git/**', '**/.vscode/**'],
+		}).map((file) => path.resolve(file));
+	} catch {
+		return [];
+	}
+}
+
+export function getFileName(filePath: string): string {
+	return path.basename(filePath);
+}
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,4 @@
+export * as fsAsync from './fs/fileSystemAsync';
+export * as fsSync from './fs/fileSystem';
+export * as str from './string';
+export * as tsUtils from './tsUtils';
