6129c22fcd88091677945ee789364491fcf4fd68
Vinicius Borges <viniciusborgeis@gmail.com>
2024-09-02 22:26:04 -0300

    ♻️ (definitions/package.json): remove redundant name and version from package.json file ✨ (packages/definitions/src/index.ts): add exports for module, require and types in index.js and .d.ts files ♻️ (utils/package.json): format 'files' field into an array
    ♻️ (fs/fileSystem.ts): remove redundant try-catch blocks and return values, improve code readability
    ♻️ (fs/fileSystemAsync.ts): refactor functions to remove try-catch blocks, simplify error handling
    ✨ (string/index.ts): refactor function toPascalCase using more modern JavaScript features like filter, map and join
    
    ♻️ (utils/index.ts): remove unused import and redundant async keyword
    ♻️ (utils/index.ts): simplify function calls by removing unnecessary types
    ♻️ (utils/tsconfig.json): add a newline at the end of the file for better readability

--- packages/utils/src/tsUtils/index.ts
+++ packages/utils/src/tsUtils/index.ts
@@ -2,20 +2,12 @@ import * as ts from 'typescript';
 import { readFile as readFileAsync } from '../fs/fileSystemAsync';
 import { readFile } from '../fs/fileSystem';
 
-export async function getSourceFileAsync(
-	filePath: string,
-	encoding: BufferEncoding = 'utf8',
-	scriptTarget: ts.ScriptTarget = ts.ScriptTarget.ES2020,
-): Promise<ts.SourceFile> {
-	const sourceCode = (await readFileAsync(filePath, encoding)) as string;
-	return ts.createSourceFile(filePath, sourceCode, scriptTarget, true);
+export async function getSourceFileAsync(filePath: string, encoding: BufferEncoding = 'utf8', scriptTarget: ts.ScriptTarget = ts.ScriptTarget.ES2020): Promise<ts.SourceFile> {
+  const sourceCode = (await readFileAsync(filePath, encoding)) as string;
+  return ts.createSourceFile(filePath, sourceCode, scriptTarget, true);
 }
 
-export function getSourceFile(
-	filePath: string,
-	encoding: BufferEncoding = 'utf8',
-	scriptTarget: ts.ScriptTarget = ts.ScriptTarget.ES2020,
-): ts.SourceFile {
-	const sourceCode = readFile(filePath, encoding) as string;
-	return ts.createSourceFile(filePath, sourceCode, scriptTarget, true);
+export function getSourceFile(filePath: string, encoding: BufferEncoding = 'utf8', scriptTarget: ts.ScriptTarget = ts.ScriptTarget.ES2020): ts.SourceFile {
+  const sourceCode = readFile(filePath, encoding) as string;
+  return ts.createSourceFile(filePath, sourceCode, scriptTarget, true);
 }
