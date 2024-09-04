f2b0abbe4b5b46ef9ba45742c1d86ac26259db30
Vinicius Borges <viniciusborgeis@gmail.com>
2024-09-04 03:00:03 -0300

    📝 (watch.ts): add support for command options for the watch command ✅ (watch.ts): extend the functionality of the watch command by adding Rojo server and more configuration options

--- apps/cli/src/commands/watch.ts
+++ apps/cli/src/commands/watch.ts
@@ -1,15 +1,21 @@
 import { Command } from 'commander';
 import color from 'picocolors';
+import WatchAction from '@actions/watch';
 
 const description = `
 \u250C ${color.bgBlack(color.bold('👀 Watch changes in the project'))}
 \u251C ${color.bold('This command will:')}
 \u251C ${color.blue('Watch changes in the project')}
-\u2514 ${color.blue('Automatically build the project')}
+\u251C ${color.blue('Automatically build the project')}
+\u2514 ${color.blue('Automatically run Rojo server')}
 `;
 
-const watchCommand = new Command('watch').description(description).action(() => {
-  console.log('watch');
-});
+const watchCommand = new Command('watch')
+  .option('-p --path <path>', 'Path to the project, default is cubix outDir')
+  .option('-e --extensions <extensions>', 'Extensions to watch. Ex: -e ts,lua,json')
+  .option('-x --exclude <exclude>', 'Exclude paths. Ex: -x node_modules,dist')
+  .option('-s --server', 'Start Rojo server parallel to watch changes server')
+  .description(description)
+  .action(WatchAction);
 
 export default watchCommand;
