24606b341251ccfca3dd9908e18a09bff642f536
Vinicius Borges <viniciusborgeis@gmail.com>
2024-09-04 18:57:22 -0300

    ♻️ (watch.ts): add type and events options to watch command ♻️ (watch.ts): add debounce time option to watch command

--- apps/cli/src/commands/watch.ts
+++ apps/cli/src/commands/watch.ts
@@ -15,6 +15,9 @@ const watchCommand = new Command('watch')
   .option('-e --extensions <extensions>', 'Extensions to watch. Ex: -e ts,lua,json')
   .option('-x --exclude <exclude>', 'Exclude paths. Ex: -x node_modules,dist')
   .option('-s --server', 'Start Rojo server parallel to watch changes server')
+  .option('-t --type <type>', 'Type of watch to be used. Options: [standard, hash, debounce], default is standard')
+  .option('-v --events <events>', 'Events to watch. Options: [all, add, change, unlink], default is change')
+  .option<number>('-d --debounceTime <debounceTime>', 'Debounce time in milliseconds, default is 1000', Number)
   .description(description)
   .action(WatchAction);
 
