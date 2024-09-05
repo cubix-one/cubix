2e9ec7804a6d75fb89753a8ac0c662380a60f18c
Vinicius Borges <viniciusborgeis@gmail.com>
2024-09-05 02:41:03 -0300

    ♻️ (actions/serve/index.ts): refactor ServeAction function to accept props object instead of separate port and address arguments for improved maintainability

--- apps/cli/src/actions/serve/index.ts
+++ apps/cli/src/actions/serve/index.ts
@@ -2,9 +2,9 @@ import RojoServer from '@core/rojoServer';
 import * as p from '@clack/prompts';
 import color from 'picocolors';
 
-export default async function ServeAction(projectPath: string, port: number, address: string) {
+export default async function ServeAction(projectPath: string, props: { port: number; address: string }) {
   p.intro(`${color.inverse('🚀 Starting Rojo Server...')}`);
-  const { ip, port: finalPort } = await RojoServer(projectPath, port, address);
+  const { ip, port: finalPort } = await RojoServer(projectPath, props);
 
   p.log.success(`📡 Rojo Server initialized on: ${color.bold(ip)}:${color.bold(finalPort)}`);
   p.note(`You can access the server monitoring page at: ${color.bold(color.underline(`http://${ip}:${finalPort}/`))}`);
